133e729d8884b15aeebed9230b7f62753cf5e639
spidersocks <sfontaine20@ucla.edu>
2025-05-06 03:35:26 +0800

    Restructured site to include homepage for future development.

---
+++ frontend/tailwindcss4/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+      <div style={{padding: "2rem"}}>
+        <h1>Sean Fontaine</h1>
+        <p>Welcome to my site! Check out my projects:</p>
+        <ul>
+          <li>
+            <a href="/800m-calculator">800m Training & Race Calculator</a>
+          </li>
+          {/* Add more links as you build more apps */}
+        </ul>
+      </div>
+    );
+  }
